cf43c5d48209294cab3b6891709a65f3fc8bb55a
guaotiantang <guaotiantang@outlook.com>
2018-01-26 20:35:40 +0800

    SSRManager

--- Plugin/ban_iptables.sh
+++ Plugin/ban_iptables.sh
@@ -5,7 +5,7 @@ export PATH
 #       System Required: CentOS/Debian/Ubuntu
 #       Description: iptables 封禁 BT、PT、SPAM（垃圾邮件）和自定义端口、关键词
 #       Version: 1.0.10
-#       Blog: https://doub.io/shell-jc2/
+#       Email:guaotiantang@outlook.com
 #=================================================
 
 sh_ver="1.0.10"
@@ -428,14 +428,14 @@ Debian / Ubuntu 系统：apt-get install iptables -y"
 }
 Update_Shell(){
 	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
-	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/guaotiantang/SSRManager/master/Plugin/ban_iptables.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && exit 1
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
 		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
 		stty erase '^H' && read -p "(默认: y):" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
-			wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh && chmod +x ban_iptables.sh
+			wget -N --no-check-certificate https://raw.githubusercontent.com/guaotiantang/SSRManager/master/Plugin/ban_iptables.sh && chmod +x ban_iptables.sh
 			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
 		else
 			echo && echo "	已取消..." && echo
